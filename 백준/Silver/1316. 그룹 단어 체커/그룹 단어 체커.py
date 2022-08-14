e2fdcf83d85b07495b9cd4527c9d374dce47e18e
YoonSungJang <108854635+myNameIsYoonSungJang@users.noreply.github.com>
2022-08-14 21:05:31 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -6,16 +6,13 @@ for i in arr:
     word = i[0]
     newstr = ""
     result = True
-    if len(i) == 1:
-        cnt += 1
-        continue
-    else:
-        for j in range(1, len(i)):
-            if i[j] != word:
-                newstr += word
-                word = i[j]
-                if word in newstr:
-                    result = False
+    
+    for j in range(1, len(i)):
+        if i[j] != word:
+            newstr += word
+            word = i[j]
+            if word in newstr:
+                result = False
     if result == True:
         cnt += 1
 print(cnt)
