43015fc05fae238ffc9839511c5af6c75b0fb2f4
myNameIsYoonSungJang <108854635+myNameIsYoonSungJang@users.noreply.github.com>
2023-07-31 18:16:53 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+jum = input()
+
+if jum == 'A+':
+    print(4.3)
+elif jum == 'A0':
+    print(4.0)
+elif jum == 'A-':
+    print(3.7)
+elif jum == 'B+':
+    print(3.3)
+elif jum == 'B0':
+    print(3.0)
+elif jum == 'B-':
+    print(2.7)
+elif jum == 'C+':
+    print(2.3)
+elif jum == 'C0':
+    print(2.0)
+elif jum == 'C-':
+    print(1.7)
+elif jum == 'D+':
+    print(1.3)
+elif jum == 'D0':
+    print(1.0)
+elif jum == 'D-':
+    print(0.7)
+else:
+    print(0.0)
